61f960405b417846002b31f1d006344f11fedf3a
GaGa-Kim <wn8925@sookmyung.ac.kr>
2021-10-06 21:02:18 +0900

    [03장] 스프링 부트에서 JPA로 데이터베이스 다뤄보자 - JPA Auditing

--- 김가경/freelec-springboot2-webservice/src/main/java/com/gagyeong/book/springboot/domain/posts/Posts.java
+++ 김가경/freelec-springboot2-webservice/src/main/java/com/gagyeong/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.gagyeong.book.springboot.domain.posts;
 
+import com.gagyeong.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
