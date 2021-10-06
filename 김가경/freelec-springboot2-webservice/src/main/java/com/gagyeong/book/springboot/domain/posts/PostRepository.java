853e5e582391deedb986798d66ceab76c79673d0
GaGa-Kim <wn8925@sookmyung.ac.kr>
2021-10-06 11:53:46 +0900

    [03장] 스프링 부트에서 JPA로 데이터베이스 다뤄보자 - JPA

---
+++ 김가경/freelec-springboot2-webservice/src/main/java/com/gagyeong/book/springboot/domain/posts/PostRepository.java
@@ -0,0 +1,6 @@
+package com.gagyeong.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Posts, Long> {
+}
---
+++ 김가경/freelec-springboot2-webservice/src/test/java/com/gagyeong/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.gagyeong.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("wn8925@sookmyung.ac.kr")
+                .build());
+
+        // when
+        List<Posts> postsList = postRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
