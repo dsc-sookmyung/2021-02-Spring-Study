3a3c62981ed596d7c7def386430988c7f00c2ebf
whaeundo25 <1916388@sookmyung.ac.kr>
2021-10-08 02:54:56 +0900

    [03장 -3] JPA Auditing으로 생성시간/수정시간 자동화하기

--- freelec-springboot2-webservice/src/main/java/com/whaeun/springboot/domain/posts/Posts.java
+++ freelec-springboot2-webservice/src/main/java/com/whaeun/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.whaeun.springboot.domain.posts;
 
+import com.whaeun.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @Entity
 
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
