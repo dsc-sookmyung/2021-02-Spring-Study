907e917254f244c344f6f78c1cff2b521069d710
whaeundo25 <1916388@sookmyung.ac.kr>
2021-10-08 02:32:01 +0900

    [03 -2] 등록/수정/조회 API 만들기

--- freelec-springboot2-webservice/src/main/java/com/whaeun/springboot/domain/posts/Posts.java
+++ freelec-springboot2-webservice/src/main/java/com/whaeun/springboot/domain/posts/Posts.java
@@ -36,4 +36,9 @@ public class Posts {
         this.author = author;
     }
 
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
 }
