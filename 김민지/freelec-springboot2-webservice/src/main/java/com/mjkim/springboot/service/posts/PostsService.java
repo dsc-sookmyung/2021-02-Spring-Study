885fa751c83e5fa07652fb8cc76ae02a4af971cc
KIM MINJI <sml07113@gmail.com>
2021-10-11 15:50:06 +0900

    [03장] 등록/수정/조회 API 만들기

---
+++ 김민지/freelec-springboot2-webservice/src/main/java/com/mjkim/springboot/service/posts/PostsService.java
@@ -0,0 +1,33 @@
+package com.mjkim.springboot.service.posts;
+
+import com.mjkim.springboot.domain.posts.Posts;
+import com.mjkim.springboot.domain.posts.PostsRepository;
+import com.mjkim.springboot.web.dto.PostsResponseDto;
+import com.mjkim.springboot.web.dto.PostsSaveRequestDto;
+import com.mjkim.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
