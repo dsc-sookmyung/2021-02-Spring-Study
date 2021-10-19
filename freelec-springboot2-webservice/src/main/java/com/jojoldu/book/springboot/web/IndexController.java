8a73c300a83ca32d03a7a6ee13dee64c0e2f211a
seohyun319 <tjguselly0319@gmail.com>
2021-10-19 23:35:40 +0900

    [04장] 머스테치로 화면 구성하기 - 기본 페이지 만들기

---
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        //머스테치 스타터 덕분에 컨트롤러에서 문자열 반환할 때 앞 경로와 뒤 파일 확장자는 자동 지정
+        return "index";  // src/main/resources/templates/index.mustache로 전환돼 view resolver가 처리
+    }
+}
