e1d9faddda0c0b49ae095147fabbdfcb332ba83c
s0hyuneee <s09glee@gmail.com>
2021-10-31 10:26:37 +0900

    [05] 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기

--- 정소현/src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
+++ 정소현/src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
@@ -1,13 +1,21 @@
 package com.jojoldu.book.springboot.web.dto;
 
+import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.PostsApiController;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index() {
@@ -19,13 +27,21 @@ public class IndexController {
         return "posts-save";
     }
 
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
 
-        PostsApiController postsService = null;
-
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post", dto);
-        return "posts-update";
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
     }
 }
