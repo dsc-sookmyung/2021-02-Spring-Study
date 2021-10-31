d24d21c0044d7a52ab89776d5053de6f4d2cd7e6
seohyun319 <tjguselly0319@gmail.com>
2021-10-31 17:10:57 +0900

    [05장] 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기 - 구글 로그인 연동하기 - 로그인 테스트

--- freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {  //Model: 서버 템플릿 엔진에서 사용할 수 있는 객체 저장 가능.
         // 여기선 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) { //세션에 저장된 값이 있으면 model에 userName으로 등록
+            model.addAttribute("userName", user.getName());
+        }
         //머스테치 스타터 덕분에 컨트롤러에서 문자열 반환할 때 앞 경로와 뒤 파일 확장자는 자동 지정
         return "index";  // src/main/resources/templates/index.mustache로 전환돼 view resolver가 처리
     }
