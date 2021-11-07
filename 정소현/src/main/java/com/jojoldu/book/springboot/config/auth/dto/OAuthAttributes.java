3ef3f02f999fae53201a74c961eecc3c27122c1a
s0hyuneee <s09glee@gmail.com>
2021-11-07 22:42:39 +0900

    pull request

--- 정소현/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ 정소현/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -32,7 +32,7 @@ public class OAuthAttributes {
         if("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
         }
-        return ofGoogle(userNameAttributeName, attributes);
+        return ofGoogle(userNameAttributeName, attributes);;
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
